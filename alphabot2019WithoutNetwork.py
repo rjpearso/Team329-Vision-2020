853a8e23912f38b4b4ae5aad73487d6458040ff3
rybradlax <34609432+rybradlax@users.noreply.github.com>
2020-01-28 15:36:31 -0500

    Update alphabot2019WithoutNetwork.py

--- alphabot2019WithoutNetwork.py
+++ alphabot2019WithoutNetwork.py
@@ -164,7 +164,7 @@ while True:
                         centerOffset = round((ce[0] - 320),1)
                         ang = ang*-1
                        # print(str(centerOffset) + " Pixels to the right away from center.")
-                    else:
+                    elif (ce[0]-320) <= 5 or (ce[0]-320) >= -5:
                         print("Perfect Angle")
 
                     dist1 = (ang/320)*centerOffset
